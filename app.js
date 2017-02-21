23347475cf6125bc429025c95947530c0820908d
Maxim Gurkin <maxim.gurkin@autodesk.com>
2017-02-22 00:16:01 +0800

    Name updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.post('/', function(req, res) {
-    memcached.set('name', req.body.name, 10, function (err) {
+    memcached.set('name', req.body.name, 60 * 5, function (err) {
         if(err) console.log(err);
         res.render('index', {name: req.body.name});
     });
